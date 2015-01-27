79d3960c400e5de623e379fbb3dd9ba31e53e242
fanbaizhou <fbzhh007@gmail.com>
2015-01-27 15:34:48 +0800

    add pan animation

--- GuessMusic/app/src/main/java/com/bigfat/guessmusic/ui/MainActivity.java
+++ GuessMusic/app/src/main/java/com/bigfat/guessmusic/ui/MainActivity.java
@@ -2,40 +2,142 @@ package com.bigfat.guessmusic.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.bigfat.guessmusic.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    //控件
+    private ImageButton mBtnPlayStart;//播放按钮
+    private ImageView mViewPan;//唱片盘片
+    private ImageView mViewPanBar;//唱片拨杆
+
+    //唱片相关动画
+    private Animation mPanAnim;
+    private LinearInterpolator mPanLin;
+
+    private Animation mBarInAnim;
+    private LinearInterpolator mBarInLin;
+
+    private Animation mBarOutAnim;
+    private LinearInterpolator mBarOutLin;
+
+    private boolean mIsRunning = false;//动画是否正在播放
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAnim();
+        initAnimListener();
+        initView();
+        initViewListener();
     }
 
+    private void initAnim() {
+        mPanAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        mPanLin = new LinearInterpolator();
+        mPanAnim.setInterpolator(mPanLin);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        mBarInAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_45);
+//        mBarInAnim.setFillAfter(true);
+        mBarInLin = new LinearInterpolator();
+        mBarInAnim.setInterpolator(mBarInLin);
+
+        mBarOutAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_d_45);
+//        mBarOutAnim.setFillAfter(true);
+        mBarOutLin = new LinearInterpolator();
+        mBarOutAnim.setInterpolator(mBarOutLin);
+    }
+
+    private void initAnimListener() {
+        mPanAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mViewPanBar.startAnimation(mBarOutAnim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mBarInAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mViewPan.startAnimation(mPanAnim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mBarOutAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mIsRunning = false;
+                mBtnPlayStart.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        mBtnPlayStart = (ImageButton) findViewById(R.id.btn_play_start);
+        mViewPan = (ImageView) findViewById(R.id.imageView1);
+        mViewPanBar = (ImageView) findViewById(R.id.imageView2);
+    }
+
+    private void initViewListener() {
+        mBtnPlayStart.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_play_start:
+                if (mViewPanBar != null) {
+                    if (!mIsRunning) {
+                        mViewPanBar.startAnimation(mBarInAnim);
+                        mIsRunning = true;
+                        mBtnPlayStart.setVisibility(View.INVISIBLE);
+                    }
+                }
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onPause() {
+        mViewPan.clearAnimation();
+        super.onPause();
     }
 }
