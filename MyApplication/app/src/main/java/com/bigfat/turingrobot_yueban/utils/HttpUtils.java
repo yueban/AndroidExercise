5ce076f4d487032ceb8b850afbf007ec98cb1945
fanbaizhou <fbzhh007@gmail.com>
2015-02-10 15:36:55 +0800

    add application ViewPagerIndicatorTest

--- MyApplication/app/src/main/java/com/bigfat/turingrobot_yueban/utils/HttpUtils.java
+++
@@ -1,8 +0,0 @@
-package com.bigfat.turingrobot_yueban.utils;
-
-/**
- * @author <a href="mailto:fbzhh007@gmail.com">bigfat</a>
- * @since 2015/1/23
- */
-public class HttpUtils {
-}
--- ViewPagerIndicatorTest/app/src/main/java/com/bigfat/viewpagerindicatortest/MainActivity.java
+++ ViewPagerIndicatorTest/app/src/main/java/com/bigfat/viewpagerindicatortest/MainActivity.java
@@ -1,13 +1,10 @@
 package com.bigfat.viewpagerindicatortest;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
 import com.viewpagerindicator.TabPageIndicator;
-
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,7 +12,6 @@ public class MainActivity extends ActionBarActivity {
     private ViewPager mViewPager;
     private TabPageIndicator mTabPageIndicator;
     private TabAdapter mTabAdapter;
-    private List<Fragment> mFragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
