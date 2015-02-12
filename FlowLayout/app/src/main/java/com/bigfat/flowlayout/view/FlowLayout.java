478e1cec67c5165b37d52eaa47fbeca2c90a90c0
fanbaizhou <fbzhh007@gmail.com>
2015-02-12 17:23:44 +0800

    add FlowLayout的测量，即onMeasure方法

---
+++ FlowLayout/app/src/main/java/com/bigfat/flowlayout/view/FlowLayout.java
@@ -0,0 +1,99 @@
+package com.bigfat.flowlayout.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author <a href="mailto:fbzhh007@gmail.com">bigfat</a>
+ * @since 2015/2/12
+ */
+public class FlowLayout extends ViewGroup {
+
+    public static final String TAG = "FlowLayout";
+
+    public FlowLayout(Context context) {
+        this(context, null);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //MeasureSpec.EXACTLY测量模式
+        int sizeWith = MeasureSpec.getSize(widthMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+        Log.i(TAG, "sizeWith--->" + String.valueOf(sizeWith));
+        Log.i(TAG, "sizeHeight--->" + String.valueOf(sizeHeight));
+//        Log.i(TAG, String.valueOf(modeWidth == MeasureSpec.EXACTLY));
+
+        //MeasureSpec.AT_MOST测量模式，即宽/高设置为wrap_content时
+        int width = 0;
+        int height = 0;
+        //记录每一行的宽度与高度
+        int lineWidth = 0;
+        int lineHeight = 0;
+        //得到内部元素的个数
+        int cCount = getChildCount();
+        for (int i = 0; i < cCount; i++) {
+            //得到子View
+            View child = getChildAt(i);
+            //测量子View
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            //得到LayoutParams，这里之所以是MarginLayoutParams，是因为在generateLayoutParams方法中返回的是MarginLayoutParams
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            //子View占据的宽度
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            //子View占据的高度
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+            //如果改行已有控件占满一行，则换行
+            if (lineWidth + childWidth > sizeWith) {
+                //取最宽的一行的宽度作为FlowLayout的宽度
+                width = Math.max(width, lineWidth);
+                //高度叠加
+                height += lineHeight;
+
+                //重置新的一行的行宽，行高
+                lineWidth = childWidth;
+                lineHeight = childHeight;
+            } else {//不换行
+                //宽度叠加
+                lineWidth += childWidth;
+                //高度取最大值
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            //如果是最后一个控件，要记录行宽与行高，因为不论换不换行，都会漏记/不对比最后一个控件的宽高
+            if (i == cCount - 1) {
+                width = Math.max(lineWidth, childWidth);
+                height += lineHeight;
+            }
+        }
+        //根据测量模式设置宽高
+        setMeasuredDimension(modeWidth == MeasureSpec.AT_MOST ? width : sizeWith,
+                modeHeight == MeasureSpec.AT_MOST ? height : sizeHeight);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    /**
+     * 与当前ViewGroup对应的LayoutParams
+     */
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+}
