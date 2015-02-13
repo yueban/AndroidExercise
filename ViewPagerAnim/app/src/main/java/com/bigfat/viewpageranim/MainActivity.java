9b96c1c2219ec328d4a92f4a2ad34fd52ffe9ef1
fanbaizhou <fbzhh007@gmail.com>
2015-02-13 15:14:44 +0800

    add 实现3.0以上ViewPager切换动画

--- ViewPagerAnim/app/src/main/java/com/bigfat/viewpageranim/MainActivity.java
+++ ViewPagerAnim/app/src/main/java/com/bigfat/viewpageranim/MainActivity.java
@@ -1,16 +1,59 @@
 package com.bigfat.viewpageranim;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private ViewPager mViewPager;
+    private int[] mImgIds = {
+            R.mipmap.guide_image1,
+            R.mipmap.guide_image2,
+            R.mipmap.guide_image3,};
+    private List<ImageView> mImages = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mViewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(mImgIds[position]);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                container.addView(imageView);
+                mImages.add(imageView);
+                return imageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(mImages.get(position));
+            }
+
+            @Override
+            public int getCount() {
+                return mImgIds.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object o) {
+                return view == o;
+            }
+        });
     }
 }
