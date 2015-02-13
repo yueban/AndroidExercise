38438944c7e20e48471d9f890e39b40db8162e75
fanbaizhou <fbzhh007@gmail.com>
2015-02-13 21:48:54 +0800

    add 闪屏开关，调色模块

--- SplashScreen/app/src/main/java/com/bigfat/splashscreen/MainActivity.java
+++ SplashScreen/app/src/main/java/com/bigfat/splashscreen/MainActivity.java
@@ -4,15 +4,32 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
+    private RelativeLayout mRlmainBg;
+    private ImageButton mBtnStartStop;
+    private ImageButton mBtnShare;
+    private ImageButton mBtnChangeColor;
+    private LinearLayout mLlColorBarContainer;
+    private SeekBar mSeekBarRed;
+    private SeekBar mSeekBarGreen;
+    private SeekBar mSeekBarBlue;
+
+    private Timer mTimer;
+
+    private int defaultColor;
     private int splashColor = Color.RED;
+
+    private boolean isSplash = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +42,111 @@ public class MainActivity extends ActionBarActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
-        final RelativeLayout mainBg = (RelativeLayout) findViewById(R.id.id_main_bg);
-        TimerTask timerTask = new TimerTask() {
-            private boolean isBlue = true;
+        defaultColor = getResources().getColor(R.color.main_bg_color);
 
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (isBlue) {
-                            mainBg.setBackgroundColor(Color.BLUE);
-                        } else {
-                            mainBg.setBackgroundColor(splashColor);
+        initView();
+        initViewListener();
+        initColorBar();
+    }
+
+    private void initView() {
+        mRlmainBg = (RelativeLayout) findViewById(R.id.id_main_bg);
+        mBtnStartStop = (ImageButton) findViewById(R.id.id_btn_start_stop);
+        mBtnShare = (ImageButton) findViewById(R.id.id_btn_share);
+        mBtnChangeColor = (ImageButton) findViewById(R.id.id_btn_change_color);
+        mLlColorBarContainer = (LinearLayout) findViewById(R.id.id_ll_color_bar_container);
+        mSeekBarRed = (SeekBar) findViewById(R.id.id_seek_bar_red);
+        mSeekBarGreen = (SeekBar) findViewById(R.id.id_seek_bar_green);
+        mSeekBarBlue = (SeekBar) findViewById(R.id.id_seek_bar_blue);
+    }
+
+    private void initViewListener() {
+        mBtnStartStop.setOnClickListener(this);
+        mBtnShare.setOnClickListener(this);
+        mBtnChangeColor.setOnClickListener(this);
+        mSeekBarRed.setOnSeekBarChangeListener(this);
+        mSeekBarGreen.setOnSeekBarChangeListener(this);
+        mSeekBarBlue.setOnSeekBarChangeListener(this);
+    }
+
+    private void initColorBar() {
+        mSeekBarRed.setProgress(Color.red(splashColor));
+        mSeekBarGreen.setProgress(Color.green(splashColor));
+        mSeekBarBlue.setProgress(Color.blue(splashColor));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.id_btn_start_stop://开始/停止
+                startOrStop();
+                break;
+
+            case R.id.id_btn_share://分享到微信
+
+                break;
+
+            case R.id.id_btn_change_color://调整闪烁颜色
+                mLlColorBarContainer.setVisibility(mLlColorBarContainer.getVisibility() == View.VISIBLE ?
+                        View.INVISIBLE : View.VISIBLE);
+                break;
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        splashColor = Color.rgb(mSeekBarRed.getProgress(), mSeekBarGreen.getProgress(), mSeekBarBlue.getProgress());
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    private void startOrStop() {
+        if (isSplash) {
+            stop();
+        } else {
+            start();
+        }
+    }
+
+    private void start() {
+        if (mTimer == null) {
+            mTimer = new Timer();
+            TimerTask mTimerTask = new TimerTask() {
+                private boolean isDefault = false;
+
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (!isSplash) {
+                                return;
+                            }
+                            if (isDefault) {
+                                mRlmainBg.setBackgroundColor(defaultColor);
+                            } else {
+                                mRlmainBg.setBackgroundColor(splashColor);
+                            }
+                            isDefault = !isDefault;
                         }
-                        isBlue = !isBlue;
-                    }
-                });
-            }
-        };
-        Timer timer = new Timer();
-        timer.schedule(timerTask, 0, 200);
+                    });
+                }
+            };
+            mTimer.schedule(mTimerTask, 0, 100);
+        }
+        isSplash = true;
+    }
+
+    private void stop() {
+        isSplash = false;
+        mRlmainBg.setBackgroundColor(defaultColor);
     }
 }
