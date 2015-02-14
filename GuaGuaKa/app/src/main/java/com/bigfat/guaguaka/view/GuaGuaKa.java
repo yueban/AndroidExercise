d08a9102c6cab16e514ced7b6bc5184658d287a6
fanbaizhou <fbzhh007@gmail.com>
2015-02-14 16:13:12 +0800

    实现简单的文本刮奖信息的刮刮卡效果

--- GuaGuaKa/app/src/main/java/com/bigfat/guaguaka/view/GuaGuaKa.java
+++ GuaGuaKa/app/src/main/java/com/bigfat/guaguaka/view/GuaGuaKa.java
@@ -2,18 +2,16 @@ package com.bigfat.guaguaka.view;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-
-import com.bigfat.guaguaka.R;
 
 /**
  * @author <a href="mailto:fbzhh007@gmail.com">bigfat</a>
@@ -32,7 +30,13 @@ public class GuaGuaKa extends View {
     private int mLastY;
 
     //刮刮卡真实内容
-    private Bitmap bitmap;
+//    private Bitmap bitmap;
+    //刮奖文本
+    private String mText;
+    private Paint mTextPaint;
+    private int mTextSize;
+    //记录刮奖文本信息的宽高
+    private Rect mTextBound;
 
     public GuaGuaKa(Context context) {
         this(context, null);
@@ -58,7 +62,9 @@ public class GuaGuaKa extends View {
         mCanvas.drawColor(0xffc0c0c0);
         //设置画笔属性
         setOuterPaint();
+        setTextPaint();
     }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -93,18 +99,35 @@ public class GuaGuaKa extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, 0, 0, null);
+//        canvas.drawBitmap(bitmap, 0, 0, null);
+        //刮奖信息
+        canvas.drawText(mText, getWidth() / 2 - mTextBound.width() / 2, getHeight() / 2 + mTextBound.height() / 2, mTextPaint);
+        //刮开区域
         drawPath();
         canvas.drawBitmap(mBitmap, 0, 0, null);
     }
 
+    /**
+     * 绘制覆盖层刮开的路径
+     */
     private void drawPath() {
         mOuterPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
         mCanvas.drawPath(mPath, mOuterPaint);
     }
 
+    private void init() {
+        mOuterPaint = new Paint();
+        mPath = new Path();
+//        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.t2);
+
+        mText = "谢谢惠顾";
+        mTextPaint = new Paint();
+        mTextBound = new Rect();
+        mTextSize = 36;
+    }
+
     /**
-     * 设置画笔属性
+     * 设置覆盖层画笔属性
      */
     private void setOuterPaint() {
         mOuterPaint.setColor(Color.RED);
@@ -116,9 +139,14 @@ public class GuaGuaKa extends View {
         mOuterPaint.setStrokeWidth(20);
     }
 
-    private void init() {
-        mOuterPaint = new Paint();
-        mPath = new Path();
-        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.t2);
+    /**
+     * 设置刮奖信息画笔属性
+     */
+    private void setTextPaint() {
+        mTextPaint.setColor(Color.DKGRAY);
+        mTextPaint.setStyle(Paint.Style.FILL);
+        mTextPaint.setTextSize(mTextSize);
+        //获得当前画笔绘制文本的宽高
+        mTextPaint.getTextBounds(mText, 0, mText.length(), mTextBound);
     }
 }
