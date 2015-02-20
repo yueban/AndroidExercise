5a1510d6a33af4a188b4c02deea8ff2f0b9a713b
fanbaizhou <fbzhh007@gmail.com>
2015-02-21 00:08:37 +0800

    微信分享成功后返回应用finish掉WXEntryActivity

--- SplashScreen/app/src/main/java/com/bigfat/splashscreen/wxapi/WXEntryActivity.java
+++ SplashScreen/app/src/main/java/com/bigfat/splashscreen/wxapi/WXEntryActivity.java
@@ -2,12 +2,9 @@ package com.bigfat.splashscreen.wxapi;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.bigfat.splashscreen.MainActivity;
-import com.bigfat.splashscreen.R;
 import com.tencent.mm.sdk.modelbase.BaseReq;
 import com.tencent.mm.sdk.modelbase.BaseResp;
 import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
@@ -17,16 +14,7 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
-        if (MainActivity.iwxapi != null) {
-            MainActivity.iwxapi.handleIntent(intent, this);
-        }
+        MainActivity.iwxapi.handleIntent(getIntent(), this);
     }
 
     @Override
@@ -35,24 +23,22 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
 
     @Override
     public void onResp(BaseResp baseResp) {
-        int result = 0;
-
-        switch (baseResp.errCode) {
-            case BaseResp.ErrCode.ERR_OK:
-                result = R.string.errcode_success;
-                break;
-            case BaseResp.ErrCode.ERR_USER_CANCEL:
-                result = R.string.errcode_cancel;
-                break;
-            case BaseResp.ErrCode.ERR_AUTH_DENIED:
-                result = R.string.errcode_deny;
-                break;
-            default:
-                result = R.string.errcode_unknown;
-                break;
-        }
-
-        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+//        String result;
+//        switch (baseResp.errCode) {
+//            case BaseResp.ErrCode.ERR_OK:
+//                result = getResources().getString(R.string.errcode_success);
+//                break;
+//            case BaseResp.ErrCode.ERR_USER_CANCEL:
+//                result = getResources().getString(R.string.errcode_cancel);
+//                break;
+//            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+//                result = getResources().getString(R.string.errcode_deny);
+//                break;
+//            default:
+//                result = getResources().getString(R.string.errcode_unknown);
+//                break;
+//        }
+//        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
         finish();
     }
 }
