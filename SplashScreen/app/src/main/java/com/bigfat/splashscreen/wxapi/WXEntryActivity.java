d377325e64e9b75748eed01eb661db001938f3e2
fanbaizhou <fbzhh007@gmail.com>
2015-02-28 16:06:44 +0800

    实现友盟更新功能，精简资源文件

--- SplashScreen/app/src/main/java/com/bigfat/splashscreen/wxapi/WXEntryActivity.java
+++ SplashScreen/app/src/main/java/com/bigfat/splashscreen/wxapi/WXEntryActivity.java
@@ -41,22 +41,6 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
 
     @Override
     public void onResp(BaseResp baseResp) {
-//        String result;
-//        switch (baseResp.errCode) {
-//            case BaseResp.ErrCode.ERR_OK:
-//                result = getResources().getString(R.string.errcode_success);
-//                break;
-//            case BaseResp.ErrCode.ERR_USER_CANCEL:
-//                result = getResources().getString(R.string.errcode_cancel);
-//                break;
-//            case BaseResp.ErrCode.ERR_AUTH_DENIED:
-//                result = getResources().getString(R.string.errcode_deny);
-//                break;
-//            default:
-//                result = getResources().getString(R.string.errcode_unknown);
-//                break;
-//        }
-//        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
         UMPlatformData platform = new UMPlatformData(UMPlatformData.UMedia.WEIXIN_FRIENDS, Util.getDeviceInfo(this));
         MobclickAgent.onSocialEvent(this, platform);
 
