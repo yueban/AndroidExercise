4a2757ecf42b1809ef91c3e4f1a30be85975e40a
fanbaizhou <fbzhh007@gmail.com>
2015-03-03 19:49:02 +0800

    完成实际测试案例

--- LockPattern/app/src/main/java/com/bigfat/lockpattern/LockActivity.java
+++ LockPattern/app/src/main/java/com/bigfat/lockpattern/LockActivity.java
@@ -1,21 +1,35 @@
 package com.bigfat.lockpattern;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.bigfat.lockpattern.view.LockPatternView;
 
 
 public class LockActivity extends ActionBarActivity {
 
+    private static final String TAG = "LockActivity";
+
     private LockPatternView mLockPattern;
+    private String mPassword;
+    /**
+     * 密码是否有效
+     */
+    private boolean isPasswordEnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
+
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        mPassword = sp.getString("password", "");
+        Log.i(TAG, "mPassword--->" + mPassword);
+        isPasswordEnable = !TextUtils.isEmpty(mPassword) && mPassword.length() >= 5;
 
         mLockPattern = (LockPatternView) findViewById(R.id.id_lock_pattern);
 
@@ -23,9 +37,25 @@ public class LockActivity extends ActionBarActivity {
             @Override
             public void onPatternChange(String password) {
                 if (!TextUtils.isEmpty(password)) {
-                    Toast.makeText(LockActivity.this, password, Toast.LENGTH_SHORT).show();
+                    if (isPasswordEnable) {
+                        if (password.equals(mPassword)) {
+                            goToMainActivity();
+                        }
+                    } else {
+                        getPreferences(MODE_PRIVATE).edit().putString("password", password).apply();
+                        goToMainActivity();
+                    }
                 }
             }
         });
+    }
+
+    /**
+     * 跳转到MainActivity
+     */
+    private void goToMainActivity() {
+        Intent intent = new Intent(LockActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ LockPattern/app/src/main/java/com/bigfat/lockpattern/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bigfat.lockpattern;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * @author <a href="mailto:fbzhh007@gmail.com">bigfat</a>
+ * @since 2015/3/3
+ */
+public class MainActivity extends ActionBarActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
