c4791b54b84c7a5006d028926cb2f36958fff1bc
fanbaizhou <fbzhh007@gmail.com>
2015-03-17 17:49:49 +0800

    Volley加载图片的几种实现

--- VolleyTest/app/src/main/java/com/bigfat/volleytest/MainActivity.java
+++ VolleyTest/app/src/main/java/com/bigfat/volleytest/MainActivity.java
@@ -1,15 +1,20 @@
 package com.bigfat.volleytest;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -28,7 +33,10 @@ public class MainActivity extends ActionBarActivity {
     public static final String url = "https://raw.githubusercontent.com/yueban/AndroidExercise/master/VolleyTest/app/src/main/java/com/bigfat/volleytest/MainActivity.java";
     public static final String url_post = "http://webservice.ncuhome.cn/NcuhomeUS.asmx/getEmployeeInfoByEmp_Name";
     public static final String url_post_mingdao = "https://api2.mingdao.com/oauth2/access_token";
+    public static final String url_img = "http://img1.gamersky.com/image2015/03/20150316ydx_5/gamersky_014origin_027_20153161758A83.jpg";
 
+    private ImageView imgMain;
+    private NetworkImageView netImgMain;
     private RequestQueue requestQueue;
 
     @Override
@@ -36,16 +44,22 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+
         requestQueue = Volley.newRequestQueue(MainActivity.this);
-        try {
-//            test1();
-//            test2();
-//            test3();
-            test4();
-            test5();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        test1();
+//        test2();
+//        test3();
+//        test4();
+//        test5();
+//        test6();
+//        test7();
+//        test8();
+    }
+
+    private void initView() {
+        imgMain = (ImageView) findViewById(R.id.img_main);
+        netImgMain = (NetworkImageView) findViewById(R.id.netImg_main);
     }
 
     private void test1() {
@@ -121,7 +135,7 @@ public class MainActivity extends ActionBarActivity {
             public void onErrorResponse(VolleyError error) {
                 Log.e("TAG", error.getMessage(), error);
             }
-        }){
+        }) {
             @Override
             public String getBodyContentType() {
                 return "application/json; charset=utf-8";
@@ -157,5 +171,33 @@ public class MainActivity extends ActionBarActivity {
             }
         };
         requestQueue.add(stringRequest);
+    }
+
+    private void test6() {
+        ImageRequest imageRequest = new ImageRequest(url_img, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                imgMain.setImageBitmap(response);
+            }
+        }, 0, 0, ImageView.ScaleType.FIT_CENTER, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                imgMain.setImageResource(R.mipmap.ic_launcher);
+            }
+        });
+        requestQueue.add(imageRequest);
+    }
+
+    private void test7() {
+        ImageLoader imageLoader = new ImageLoader(requestQueue, new BitmapCache());
+        ImageLoader.ImageListener imageListener = imageLoader.getImageListener(imgMain, R.mipmap.ic_launcher, R.mipmap.ic_launcher);
+        imageLoader.get(url_img, imageListener, 200, 200);
+    }
+
+    private void test8() {
+        ImageLoader imageLoader = new ImageLoader(requestQueue, new BitmapCache());
+        netImgMain.setDefaultImageResId(R.mipmap.ic_launcher);
+        netImgMain.setErrorImageResId(R.mipmap.ic_launcher);
+        netImgMain.setImageUrl(url_img, imageLoader);
     }
 }
