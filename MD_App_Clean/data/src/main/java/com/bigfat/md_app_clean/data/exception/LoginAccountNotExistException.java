92ff12973911dbc1ed4febe1eb6f0b65fe5dffbc
yueban <fbzhh007@gmail.com>
2016-03-22 10:19:09 +0800

    remove MD_App_Clean

--- MD_App_Clean/data/src/main/java/com/bigfat/md_app_clean/data/exception/LoginAccountNotExistException.java
+++
@@ -1,9 +0,0 @@
-package com.bigfat.md_app_clean.data.exception;
-
-/**
- * Created by yueban on 22:23 3/3/16.
- * Email: fbzhh007@gmail.com
- * QQ: 343278606
- */
-public class LoginAccountNotExistException extends RuntimeException {
-}
