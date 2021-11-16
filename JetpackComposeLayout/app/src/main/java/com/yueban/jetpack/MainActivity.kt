daa923d69e87707baccd2a62419fd2a4cb2320ee
yueban <fbzhh007@gmail.com>
2021-11-16 11:12:49 +0800

    feature(jetpackcompose): add Intrinsics demo

--- JetpackComposeLayout/app/src/main/java/com/yueban/jetpack/MainActivity.kt
+++ JetpackComposeLayout/app/src/main/java/com/yueban/jetpack/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
@@ -26,6 +30,7 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
@@ -322,6 +327,25 @@ fun ConstraintLayoutContent2() {
     }
 }
 
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(modifier = Modifier
+            .weight(1f)
+            .padding(start = 4.dp)
+            .wrapContentWidth(Alignment.Start),
+             text = text1)
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(modifier = Modifier
+            .weight(1f)
+            .padding(end = 4.dp)
+            .wrapContentWidth(Alignment.End),
+             text = text2)
+    }
+}
+
 @ExperimentalCoilApi
 @Composable
 fun ImageItem(index: Int) {
@@ -395,5 +419,7 @@ fun MainView() {
 
 //    StaggeredGridDemo()
 
-    ConstraintLayoutContent2()
+//    ConstraintLayoutContent2()
+
+    TwoTexts(text1 = "Hello", text2 = "World")
 }
