f1815e223c983e8b4c54dc23ea77294e2c089a6d
yueban <fbzhh007@gmail.com>
2021-12-28 19:27:50 +0800

    feature(jetpackcompose): add navigation start code

---
+++ JetpackComposeDemo/navigation/src/main/java/com/yueban/jetpack/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.yueban.jetpack.ui.theme
+
+import androidx.compose.material.darkColors
+import androidx.compose.ui.graphics.Color
+
+val Green500 = Color(0xFF1EB980)
+val DarkBlue900 = Color(0xFF26282F)
+
+// Rally is always dark themed.
+val ColorPalette = darkColors(
+    primary = Green500,
+    surface = DarkBlue900,
+    onSurface = Color.White,
+    background = DarkBlue900,
+    onBackground = Color.White
+)
---
+++ JetpackComposeDemo/navigation/src/main/java/com/yueban/jetpack/ui/theme/Theme.kt
@@ -0,0 +1,48 @@
+package com.yueban.jetpack.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+/**
+ * A [MaterialTheme] for Rally.
+ */
+@Composable
+fun RallyTheme(content: @Composable () -> Unit) {
+
+    MaterialTheme(colors = ColorPalette, typography = Typography, content = content)
+}
+
+/**
+ * A theme overlay used for dialogs.
+ */
+@Composable
+fun RallyDialogThemeOverlay(content: @Composable () -> Unit) {
+    // Rally is always dark themed.
+    val dialogColors = darkColors(
+        primary = Color.White,
+        surface = Color.White.copy(alpha = 0.12f).compositeOver(Color.Black),
+        onSurface = Color.White
+    )
+
+    // Copy the current [Typography] and replace some text styles for this theme.
+    val currentTypography = MaterialTheme.typography
+    val dialogTypography = currentTypography.copy(
+        body2 = currentTypography.body1.copy(
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 1.sp
+        ),
+        button = currentTypography.button.copy(
+            fontWeight = FontWeight.Bold,
+            letterSpacing = 0.2.em
+        )
+    )
+    MaterialTheme(colors = dialogColors, typography = dialogTypography, content = content)
+}
