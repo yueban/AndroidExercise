1143a1fc7c1a3af1e5265a5fc4f5e9adb79d7478
yueban <fbzhh007@gmail.com>
2022-04-06 11:50:15 +0800

    feature(jetpackcompose): add migration start code

---
+++ JetpackComposeDemo/migration/src/main/java/com/yueban/jetpack/utilities/InjectorUtils.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yueban.jetpack.utilities
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.yueban.jetpack.data.AppDatabase
+import com.yueban.jetpack.data.GardenPlantingRepository
+import com.yueban.jetpack.data.PlantRepository
+import com.yueban.jetpack.viewmodels.GardenPlantingListViewModelFactory
+import com.yueban.jetpack.viewmodels.PlantDetailViewModelFactory
+import com.yueban.jetpack.viewmodels.PlantListViewModelFactory
+
+/**
+ * Static methods used to inject classes needed for various Activities and Fragments.
+ */
+object InjectorUtils {
+
+    private fun getPlantRepository(context: Context): PlantRepository {
+        return PlantRepository.getInstance(
+            AppDatabase.getInstance(context.applicationContext).plantDao()
+        )
+    }
+
+    private fun getGardenPlantingRepository(context: Context): GardenPlantingRepository {
+        return GardenPlantingRepository.getInstance(
+            AppDatabase.getInstance(context.applicationContext).gardenPlantingDao()
+        )
+    }
+
+    fun provideGardenPlantingListViewModelFactory(
+        context: Context
+    ): GardenPlantingListViewModelFactory {
+        return GardenPlantingListViewModelFactory(getGardenPlantingRepository(context))
+    }
+
+    fun providePlantListViewModelFactory(fragment: Fragment): PlantListViewModelFactory {
+        return PlantListViewModelFactory(getPlantRepository(fragment.requireContext()), fragment)
+    }
+
+    fun providePlantDetailViewModelFactory(
+        context: Context,
+        plantId: String
+    ): PlantDetailViewModelFactory {
+        return PlantDetailViewModelFactory(
+            getPlantRepository(context),
+            getGardenPlantingRepository(context), plantId
+        )
+    }
+}
